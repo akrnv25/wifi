29141a1a89f6ef0c64e28b1523556be4287a7d6d
korenev6052 <korenev6052@gmail.com>
2021-06-30 00:10:16 +0300

    handled wifi methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Camera } from '@ionic-native/camera/ngx';
+import { WifiWizard2 } from '@ionic-native/wifi-wizard-2/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +12,12 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+    WifiWizard2
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { WifiWizard2 } from '@ionic-native/wifi-wizard-2/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  response: string;
+
+  constructor(
+    private wifiWizard2: WifiWizard2,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+  }
+
+  ngOnInit() {
+    this.wifiWizard2.getConnectedSSID()
+      .then(response => {
+        this.response = JSON.stringify(response);
+        this.changeDetectorRef.detectChanges();
+      });
+
+    this.wifiWizard2.scan()
+      .then(response => {
+        this.response = JSON.stringify(response);
+        this.changeDetectorRef.detectChanges();
+      });
+
+    this.wifiWizard2.connect('Batman', true, '25061307', 'WPA')
+      .then(response => {
+        this.response = JSON.stringify(response);
+        this.changeDetectorRef.detectChanges();
+      });
+  }
 
 }
